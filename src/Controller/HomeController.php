56e07b4cc9c2e68836014d87b656849e5e7ab78c
basemsamir <bassem.samir@Bassems-MacBook-Pro.local>
2022-01-09 23:04:06 +0200

    showing list of categories and articles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * @Route("/home", name="home")
      */
     public function index(): Response
     {
+        $categories = $this->doctrine->getRepository(Category::class)->findAll();
+        $articles = $this->doctrine->getRepository(Article::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categories' => $categories,
+            'articles'   => $articles,
         ]);
     }
 }
