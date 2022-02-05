5ec5341856ce015525028a0f54eb8ad26ed6fd94
basemsamir <bassem.samir@Bassems-MacBook-Pro.local>
2022-02-05 19:18:53 +0200

    adding tag field to be inserted/updated in admin

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -8,6 +8,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
@@ -17,6 +19,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Component\Validator\Constraints\Choice;
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -35,10 +38,14 @@ class ArticleCrudController extends AbstractCrudController
             AssociationField::new('category')->setSortable(false),
             AssociationField::new('user','Author')
                 ->setSortable(false),
+            AssociationField::new('tags')->hideOnIndex()
+            ->setFormTypeOptionIfNotSet('by_reference', false),
             ImageField::new('image_path')->setUploadDir('public/img')->hideOnIndex(),
+
             IntegerField::new('views')->hideOnForm(),
             IntegerField::new('likes')->hideOnForm(),
-            DateTimeField::new('created')->hideOnForm()
+            DateTimeField::new('created')->hideOnForm(),
+
         ];
     }
 
