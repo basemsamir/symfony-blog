d0b5e384d57957beb86050974342c7d50f84ec69
basemsamir <bassem.samir@Bassems-MacBook-Pro.local>
2022-02-16 20:34:09 +0200

    assigning the current logged user to adding/editing articles (admin)

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,10 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use App\Repository\UserRepository;
 use Carbon\Carbon;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
@@ -23,12 +30,10 @@ use Symfony\Component\Validator\Constraints\Choice;
 
 class ArticleCrudController extends AbstractCrudController
 {
-
     public static function getEntityFqcn(): string
     {
         return Article::class;
     }
-
 
     public function configureFields(string $pageName): iterable
     {
@@ -38,7 +43,8 @@ class ArticleCrudController extends AbstractCrudController
             TextareaField::new('content')->hideOnIndex(),
             AssociationField::new('category')->setSortable(false),
             AssociationField::new('user','Author')
-                ->setSortable(false),
+                ->setSortable(false)
+                ->hideOnForm(),
             AssociationField::new('tags')->hideOnIndex()
             ->setFormTypeOptionIfNotSet('by_reference', false),
             ImageField::new('image_path')->setUploadDir('public/img')->hideOnIndex(),
@@ -58,17 +64,25 @@ class ArticleCrudController extends AbstractCrudController
 
     public function configureFilters(Filters $filters): Filters
     {
-       return $filters->add(EntityFilter::new('category'))
-           ->add(EntityFilter::new('user'));
+       return $filters->add(EntityFilter::new('category'));
     }
 
-    public function createEntity(string $entityFqcn)
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $parent_query = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        if(!in_array('ROLE_ADMIN',$this->getUser()->getRoles())){
+            $parent_query->where('entity.user = '.$this->getUser()->getId());
+        }
+        return $parent_query;
+    }
+
+    public function createEntity(string $entityFqcn): Article
     {
        $article = new Article();
        $article->setLikes(0);
        $article->setCreated(Carbon::now());
+       $article->setUser($this->getUser());
        return $article;
     }
-
 
 }
