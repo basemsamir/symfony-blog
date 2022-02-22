0a607a7a3614c7862bb4978407f8ec7308a9f108
basemsamir <bassem.samir@Bassems-MacBook-Pro.local>
2022-02-22 19:02:02 +0200

    making contact us form & sending mails

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\ContactUsFormType;
 use App\Service\ArticleService;
 use App\Service\CategoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -35,6 +39,41 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'articles'          => $articles,
             'latest_articles'   => $latest_articles,
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/contact-us",
+     *     name="contact-us"
+     * )
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    public function contact_us(Request $request, MailerInterface $mailer): Response
+    {
+        $contact_us_form = $this->createForm(ContactUsFormType::class);
+
+        $contact_us_form->handleRequest($request);
+        if ($contact_us_form->isSubmitted() && $contact_us_form->isValid()) {
+            $mail_data = $contact_us_form->getData();
+
+            $email = (new Email())
+                ->from($mail_data['email'])
+                ->to('basemlovephp@gmail.com')
+                ->subject($mail_data['subject'])
+                ->text($mail_data['message']);
+
+            $mailer->send($email);
+            $this->addFlash(
+                'notice',
+                'Your message was send!'
+            );
+            return $this->redirectToRoute('contact-us');
+        }
+
+        return $this->render('home/contact-us.html.twig',
+        [
+            'contact_us' => $contact_us_form->createView()
         ]);
     }
 }
