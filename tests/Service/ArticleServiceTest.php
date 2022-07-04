31b079e00a6c5f1471f786c23b97ea0f0a2289df
basemsamir <bassem.samir@Bassems-MacBook-Pro.local>
2022-07-04 19:50:40 +0200

    adding two more unit tests on ArticleService

--- tests/Service/ArticleServiceTest.php
+++ tests/Service/ArticleServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Service;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use App\Service\ArticleService;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -38,5 +40,37 @@ class ArticleServiceTest extends KernelTestCase
         $articles = $this->articleService->getArticlesPerPage(11, 3);
 
         $this->assertEquals(1, count($articles));
+    }
+
+    public function testGetLastArticle(): void
+    {
+        $lastArticle = [
+            "id" => "125",
+            "created" => "2022-02-16",
+            "title" => "I am admin",
+            "image_path" => "/img/212442263_10159857138972650_8165063009669159094_n.jpg",
+            "category_id" => "15",
+            "category_name" => "News",
+            "article_url" => "/article/125"
+        ];
+
+        $article = $this->articleService->getLatestArticles(1);
+
+        $this->assertEquals($lastArticle, $article[0]);
+    }
+
+    public function testIncreaseViewsOfArticle(): void
+    {
+        $articleRepo = static::getContainer()->get(ArticleRepository::class);
+
+        $article          = $articleRepo->find(125);
+        $previousViews    = $article->getViews();
+
+        $this->articleService->increaseViews($article);
+
+        $article  = $articleRepo->find(125);
+        $newViews = $article->getViews();
+
+        $this->assertEquals($previousViews+1, $newViews);
     }
 }
